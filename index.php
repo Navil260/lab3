cef3451f47970c45a61f6bfbda501cdb8a9a6586
Navil26 <hlod260@gmail.com>
2023-08-31 18:59:15 +0700

    aasd

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+for($i =1 ; $i<=20; $i++){
+    if($i % 2 == 0) 
+    echo "NUM: $i <br>";
+
+}
+?>
+</body>
+</html>
---
+++ input.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="result.php" method="get">
+    A=<input type="text" name="a"><br>
+    B=<input type="text" name="b"><br>
+    <input type="SUBMIT" value="calculated"> 
+</body>
+</html>
